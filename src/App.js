6ecdd4327906cbadd6255e0927830b18f313f4e9
michael-nevius <michael.a.nevius@gmail.com>
2022-05-10 15:02:19 -0400

    Fixed my app so now my add song is working again now i need to work on a filter/search function for my app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import DisplayLibrary from './Components/DisplayLibrary/DisplayLibrary';
-import React, { useState, useEffect } from 'react';
-
+import AddMusic from './Components/AddMusic/AddMusic'
 
 function App() {
   const [music, setMusic] = useState([]);
   
- 
-  
   useEffect(() => {
     getAllMusic();
   }, [])
-  
-  
-  
+
   async function getAllMusic(){
-    try{
-    let response = await axios.get('http://127.0.0.1:8000/api/music/');
-    setMusic(response.data);
-    console.log(response.data)
+    let response  = await axios.get('http://127.0.0.1:8000/api/music/');
+    setMusic(response.data)
+    console.log(music)
   }
-  catch (ex) {
-    console.log('there is an error', ex)
-  }
+
+  async function createMusic(newMusic){
+    let response = await axios.post('http://127.0.0.1:8000/api/music/', newMusic);
+    if(response.status === 201){
+      await getAllMusic();
+    }
   }
   return (
-    <div>
-      <div>
-        <h1>Music Library</h1>
+    <div className='Music Library'>
+      <div >
         <DisplayLibrary music={music}/>
+      </div>
+      <div>
+        <AddMusic addMusic={createMusic} />
       </div>
     </div>
   );
 }
+
 export default App;
--- src/Components/AddMusic/AddMusic.jsx
+++ src/Components/AddMusic/AddMusic.jsx
@@ -1,2 +1,53 @@
+import React, { useState } from 'react'
 
+const AddMusicForm = (props) => {
+    const [title, setTitle] = useState('')
+    const [artist, setArtist] = useState('')
+    const [album, setAlbum] = useState('')
+    const [release_date, setRelease_Date] = useState('')
+    const [genre, setGenre] = useState('')
+    
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newMusic = {
+            title: title,
+            artist: artist,
+            album: album,
+            release_date: release_date,
+            genre: genre,
+            likes: 0
+        }
+        console.log(newMusic)
+        props.addMusic(newMusic)
+    }
+
+
+return(
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label>Title</label>
+            <input type='text' value={title} onChange={(event) => setTitle(event.target.value)} />
+        </div>
+        <div>
+            <label>Artist</label>
+            <input type='text' value={artist} onChange={(event) => setArtist(event.target.value)} />
+        </div>
+        <div>
+            <label>Album</label>
+            <input type='text' value={album} onChange={(event) => setAlbum(event.target.value)} />
+        </div>
+        <div>
+            <label>Release Date</label>
+            <input type='date' value={release_date} onChange={(event) => setRelease_Date(event.target.value)} />
+        </div>
+        <div>
+            <label>Genre</label>
+            <input type='text' value={genre} onChange={(event) => setGenre(event.target.value)} />
+        </div>
+        <button type='submit'>Add New Music</button>
+    </form>
+    )
+}
+export default AddMusicForm
 
