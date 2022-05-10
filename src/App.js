8f74676cff142ab194ac764c013e237dd7e5c969
michael-nevius <michael.a.nevius@gmail.com>
2022-05-10 14:33:05 -0400

    Started from the beginning almost just introduced my axios function to DisplayLibrary + and added to the App.js so i could display my table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import DisplayLibrary from './Components/DisplayLibrary/DisplayLibrary';
-import AddMusic from './Components/AddMusic/AddMusic'
-import SearchBar from './Components/SearchBar/SearchBar';
+import React, { useState, useEffect } from 'react';
 
+
+function App() {
+  const [music, setMusic] = useState([]);
+  
+ 
+  
+  useEffect(() => {
+    getAllMusic();
+  }, [])
+  
+  
+  
+  async function getAllMusic(){
+    try{
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    setMusic(response.data);
+    console.log(response.data)
+  }
+  catch (ex) {
+    console.log('there is an error', ex)
+  }
+  }
+  return (
+    <div>
+      <div>
+        <h1>Music Library</h1>
+        <DisplayLibrary music={music}/>
+      </div>
+    </div>
+  );
+}
+export default App;
--- src/Components/DisplayLibrary/DisplayLibrary.jsx
+++ src/Components/DisplayLibrary/DisplayLibrary.jsx
@@ -3,41 +3,46 @@ import React from 'react';
 
 
 const DisplayLibrary = (props) => {
-    return (
-        <div>
-            <h3>MUSIC LIBRARY</h3>
-            <table className="table">
-            <thead>
-                <tr>
-                    <th scope='col'>#</th>
-                    <th scope='col'>Title</th>
-                    <th scope='col'>Artist</th>
-                    <th scope='col'>Album</th>
-                    <th scope='col'>Release Date</th>
-                    <th scope='col'>Genre</th>
-                    <th scope='col'>Likes</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.parentMusic.map((music, element) => {
-                    return(
-                        <tr key={element}>
-                            <td>{element +1}</td>
-                            <td>{music.title}</td>
-                            <td>{music.artist}</td>
-                            <td>{music.album}</td>
-                            <td>{music.release_date}</td>
-                            <td>{music.genre}</td>
-                            <td>{music.likes}</td>
-                            <td><button onClick={() => props.likeSong(music.id)}>Like</button></td>
-                            <td><button onClick={() => props.deleteSong(music.id)}> Delete Song</button></td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
-    </div>
-    )
-}
 
-export default DisplayLibrary
+      
+    return ( 
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                    <th>#</th>    
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                    <th>Likes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    {props.music.map((music, element) => {
+                        return (
+                            <tr key={element}>
+                                <td>{element +1}</td>
+                                <td>{music.title}</td>
+                                <td>{music.artist}</td>
+                                <td>{music.album}</td>
+                                <td>{music.release_date}</td>
+                                <td>{music.genre}</td>
+                                <td>{music.likes}</td>
+                                <td><button>like</button></td>
+                                <td><button>delete</button></td>
+                            </tr>
+                        )
+                    }
+                    )}
+             
+                </tbody>
+            </table>
+        </div>
+
+        
+    );}
+ 
+export default DisplayLibrary;
